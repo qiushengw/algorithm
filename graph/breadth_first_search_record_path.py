ca72356972c679acdc67e81f7f97764ac8f078e0
qiushengw <qiushengw@gmail.com>
2018-11-02 22:54:57 +0800

    record the path

--- graph/breadth_first_search_record_path.py
+++ graph/breadth_first_search_record_path.py
@@ -24,18 +24,40 @@ graph={
         "alice" : ["you", "peggy"]
 }
 
+from collections import deque
 
 def is_seller(name):
     return "alice" in name
 
 
+def backtrace(parent, start, end):
+    path = [end]
+    while path[-1] != start:
+        path.append(parent[path[-1]])
+    path.reverse()
 
-from collections import deque
+    return path
+
+def bfs_with_path(graph, start, end):    
+    parent ={}
+    queue=[]
+    queue.append(start)
+    while queue:
+        node = queue.pop(0)
+        if end == node:
+            backtrace(parent, start, end)
+
+        for adjacent in graph[node]:
+            if node not in queue:
+                parent[adjacent]=node
+                queue.append(adjacent)
+
+
 def bfs(graph, start, end):
     search_queue = deque()
     search_queue += graph[start]
     searched=[]    
-     
+
     while search_queue:
         print(search_queue) 
         person = search_queue.popleft();
@@ -56,7 +78,8 @@ def bfs(graph, start, end):
 
 
 
-result=bfs(graph, "anuj", "thom")
+result=bfs_with_path(graph, "anuj", "thom")
+print(result)
 
 
 
